659dddc7937235ded3a7af958ce9a487bf0ebf51
Joshua Levin <joshua.c.levin@gmail.com>
2018-04-21 16:00:31 +0100

    zones move and wrap

---
+++ Assets/Scripts/Zone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone : MonoBehaviour
+{
+	private float moveSpeed = -0.2f;
+    private Camera mainCamera;
+    private float respawnPosition;
+
+    public void Start()
+    {
+        // Store reference to main camera
+        mainCamera = Camera.main;
+        // Work out at what position the zone should resapwn itself
+        respawnPosition = mainCamera.ViewportToWorldPoint(Vector2.zero).x * 2;
+    }
+
+    public void FixedUpdate()
+    {
+        transform.Translate(new Vector2(moveSpeed, 0));
+        if (transform.position.x <= respawnPosition)
+        {
+            transform.position = new Vector2(respawnPosition * -1, 0);
+        }
+    }
+}
