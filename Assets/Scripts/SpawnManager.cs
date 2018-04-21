4dcb4c2a0180d8d3789153770436f56d5037582b
Joshua Levin <joshua.c.levin@gmail.com>
2018-04-21 20:51:21 +0100

    pseudocode spawn manager and time manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	// Does this spawn manager focus on the top or bottom floor
+	// Floor
+
+	// Rock card prefab
+	// Draw card prefab
+	// Ground enemy prefab
+	// Flying enemy prefab
+
+	// An event handler that when the timer manager ticks it spawns
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+	// The event delegate for ticks on the top floor
+	// The event delegate for ticks on the bottom floor
+	// The range of time in which a tick can occur, i.e. +/- 1 second either way
+	// The beginning average time per tick
+	// The current top floor time to the next tick
+	// The current top floor time since last tick
+	// The current bottom floor time to the next tick
+	// The current bottom floor time since last tick
+	// How long should each level be before stepping up to the next
+	// How much shorter should the average time per tick get per step
+}
