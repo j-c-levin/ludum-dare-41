a6c6aaff11bacf833735a62586cd5b5b17f88d7c
Joshua Levin <joshua.c.levin@gmail.com>
2018-04-22 23:36:48 +0100

    landing page done

---
+++ Assets/Scripts/LandingStartTutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class LandingStartTutorial : MonoBehaviour
+{
+    private float moveHeight = 30f;
+
+    public void Start()
+    {
+        animate();
+    }
+
+    public void onClick()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    private void animate()
+    {
+        DOTween.Sequence()
+        .Append(
+            transform.DOLocalMoveY(moveHeight, 2f)
+                .SetEase(Ease.InOutSine)
+        )
+        .Append(
+            transform.DOLocalMoveY(-moveHeight, 2f)
+                .SetEase(Ease.InOutSine)
+        )
+        .SetLoops(-1, LoopType.Yoyo);
+    }
+}
